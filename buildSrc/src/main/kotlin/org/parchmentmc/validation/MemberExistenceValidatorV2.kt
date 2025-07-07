660e3bb31178630bcd6091cabd82613d28840b79
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2025-07-07 19:57:20 +0200

    Update tooling

---
+++ buildSrc/src/main/kotlin/org/parchmentmc/validation/MemberExistenceValidatorV2.kt
@@ -0,0 +1,23 @@
+package org.parchmentmc.validation
+
+import org.parchmentmc.compass.data.validation.impl.MemberExistenceValidator
+import org.parchmentmc.feather.mapping.MappingDataContainer
+import org.parchmentmc.feather.metadata.ClassMetadata
+import org.parchmentmc.feather.metadata.FieldMetadata
+
+class MemberExistenceValidatorV2 : MemberExistenceValidator() {
+
+    override fun visitField(
+        classData: MappingDataContainer.ClassData, fieldData: MappingDataContainer.FieldData,
+        classMetadata: ClassMetadata?, fieldMetadata: FieldMetadata?
+    ) {
+        super.visitField(classData, fieldData, classMetadata, fieldMetadata)
+        if (fieldMetadata != null) {
+            if (fieldMetadata.descriptor.mojangName.orElseThrow() != fieldData.descriptor) {
+                error("Field descriptor does not match according to metadata.")
+            }
+        } else {
+            // Fields existence are already checked in MemberExistenceValidator
+        }
+    }
+}
